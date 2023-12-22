66556f42e696599d4bade7962754fc5dbfe63a05
ray <asirei1012@gmail.com>
2023-12-22 11:28:07 +0700

    fix: disable the ChakraProvider to display the styled trading page with Orderly Network

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -4,7 +4,10 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  // Disable ChakraProvider for now to display the Orderly TradingPage styled with the module's default styles
+  // return <ChakraProvider>{children}</ChakraProvider>;
+
+  return children;
 };
 
 export default Providers;
