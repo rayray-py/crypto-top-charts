893f7de60a1ae1b5f868aaa3f78ad2d559abf0ee
ray <asirei1012@gmail.com>
2023-12-22 11:29:17 +0700

    fix: wrap the TradingPage component with the providers of Orderly Network

--- src/app/eth/page.tsx
+++ src/app/eth/page.tsx
@@ -2,27 +2,32 @@
 
 import React from "react";
 import { useState } from "react";
-// import { TradingViewChart } from "@/components/TradingViewChart";
-import { Box } from "@chakra-ui/react";
 import "@orderly.network/react/dist/styles.css";
-import { TradingPage } from "@orderly.network/react";
+import { OrderlyAppProvider, TradingPage } from "@orderly.network/react";
+import { ConnectorProvider } from "@orderly.network/web3-onboard";
 
 const ETHChart: React.FC = () => {
   const [symbol, setSymbol] = useState("PERP_ETH_USDC");
   return (
-    <Box w="full">
-      <TradingPage
-        symbol={symbol}
-        tradingViewConfig={{
-          scriptSRC:
-            "https://unpkg.com/lightweight-charts/dist/lightweight-charts.standalone.production.js",
-          // library_path: "/assets/chart/charting_library/",
-        }}
-        onSymbolChange={(symbol) => {
-          setSymbol(symbol.symbol);
-        }}
-      />
-    </Box>
+    <ConnectorProvider apiKey="<Your web3-onboard's api key>">
+      <OrderlyAppProvider
+        networkId="testnet"
+        brokerId="<Your broker id>"
+        logoUrl="https://alice.slash.vision/images/slash_logo.png"
+      >
+        <TradingPage
+          symbol={symbol}
+          tradingViewConfig={{
+            scriptSRC:
+              "https://unpkg.com/lightweight-charts/dist/lightweight-charts.standalone.production.js",
+            // library_path: "/assets/chart/charting_library/",
+          }}
+          onSymbolChange={(symbol) => {
+            setSymbol(symbol.symbol);
+          }}
+        />
+      </OrderlyAppProvider>
+    </ConnectorProvider>
   );
 };
 
